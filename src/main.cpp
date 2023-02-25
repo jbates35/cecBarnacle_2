c5544feae98cee5c8cd34c4d12f43386fcde84a8
Faran Bhatti <faran_bhatti@hotmail.com>
2023-02-25 12:09:39 -0500

    update: IRremote library

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
 #include <Arduino.h>
+#include <IRremote.h>
+
+
+// Global declarations
+const int RECV_PIN = 7;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  // Enable the IR receiver
+  IrReceiver.begin(RECV_PIN, ENABLE_LED_FEEDBACK);
+  IrReceiver.enableIRIn();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if (IrReceiver.decode()) {
+    Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
+    delay(100);
+    IrReceiver.resume();
+  }
 }
